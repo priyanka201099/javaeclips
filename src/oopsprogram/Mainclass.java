fc483242522b21f5f773d5488b14f502044b309a
priyanka201099 <priyanka.jagdale43@gmail.com>
2025-04-10 09:37:32 +0530

    Initial commit

---
+++ src/oopsprogram/Mainclass.java
@@ -0,0 +1,31 @@
+package oopsprogram;
+
+public class Mainclass {
+
+	public static void main(String[] args) 
+	{
+     Person p1= new Person();
+     Person p2= new Person();
+     Person p3= new Person();
+     System.out.println(p2.age);
+     System.out.println(p2.fname);
+     System.out.println(p2.lname);
+      
+     Person p4= new Person(20);
+     System.out.println("Age: "+p4.age);
+     
+     Person p5 = new Person("Neha","vasaikar");
+     
+     
+	}
+
+}
+this.age=age;
+thi
+
+
+
+public class Person
+{
+	
+}
